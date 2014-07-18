d52de6367a0801cc0241d1bcdf2f61aaa6f81138
Christopher S. Case <chris.case@g33xnexus.com>
2014-07-17 20:07:04 -0500

    Added required functionality to the pages section.

--- server/database.js
+++ server/database.js
@@ -4,6 +4,8 @@
 // @module database.js
 //----------------------------------------------------------------------------------------------------------------------
 
+var Promise = require('bluebird');
+
 var db = require('./models');
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -39,6 +41,14 @@ var users = {
 // Pages
 //----------------------------------------------------------------------------------------------------------------------
 
+function _getPageID(slug)
+{
+    return db.Slug.get(slug).getJoin().run().then(function(slugInst)
+    {
+        return Promise.resolve(slugInst.currentRevision.page.id);
+    });
+} // end _getPageID
+
 var pages = {
     get: function(slug)
     {
@@ -47,32 +57,129 @@ var pages = {
 
     getHistory: function(slug)
     {
-
+        return _getPageID(slug).then(function(pageID)
+        {
+            return db.Revision.filter({ page_id: pageID })
+                .orderBy('commit.committed').getJoin().run();
+        });
     },
 
-    create: function(slug)
+    create: function(slug, page, user)
     {
+        var pageInst = new db.Page({});
 
+        return pageInst.save().then(function(pageInst)
+        {
+            var commitInst = new db.Commit({
+                message: page.commit,
+                user_id: user.email
+            });
+
+            return commitInst.save().then(function(commitInst)
+            {
+                var revInst = new db.Revision({
+                    page_id: pageInst.id,
+                    slug_id: slug,
+                    title: page.title,
+                    tags: page.tags || [],
+                    body: page.body,
+                    commit_id: commitInst.id
+                });
+
+                return revInst.save().then(function(revInst)
+                {
+                    var slugInst = new db.Slug({
+                        url: slug,
+                        currentRevision_id: revInst.id
+                    });
+
+                    return slugInst.save();
+                });
+            });
+        });
     },
 
-    update: function(slug)
+    update: function(slug, page, user)
     {
+        var commitInst = new db.Commit({
+            message: page.commit,
+            user_id: user.email
+        });
 
+        return commitInst.save().then(function(commitInst)
+        {
+            return _getPageID(slug).then(function(pageID)
+            {
+                return db.Slug.get(slug).getJoin().run().then(function(slugInst)
+                {
+                    var revInst = new db.Revision({
+                        page_id: pageID,
+                        slug_id: slug,
+                        title: page.title,
+                        tags: page.tags || [],
+                        body: page.body,
+                        commit_id: commitInst.id
+                    });
+
+                    revInst.title = revInst.title || slugInst.currentRevision.title;
+                    revInst.tags = revInst.tags || slugInst.currentRevision.tags;
+                    revInst.body = revInst.body || slugInst.currentRevision.body;
+
+                    return revInst.save().then(function(revInst)
+                    {
+                        slugInst.currentRevision_id = revInst.id;
+
+                        return slugInst.save();
+                    });
+                });
+            });
+        });
     },
 
     delete: function(slug)
     {
+        var commitInst = new db.Commit({
+            message: page.commit,
+            user_id: user.email
+        });
 
+        return commitInst.save().then(function(commitInst)
+        {
+            return _getPageID(slug).then(function(pageID)
+            {
+                return db.Slug.get(slug).run().then(function(slugInst)
+                {
+                    var revInst = new db.Revision({
+                        page_id: pageID,
+                        slug_id: slug,
+                        commit_id: commitInst.id,
+                        deleted: true
+                    });
+
+                    return revInst.save().then(function(revInst)
+                    {
+                        slugInst.currentRevision_id = revInst.id;
+
+                        return slugInst.save();
+                    });
+                });
+            });
+        });
     },
 
     move: function(slug)
     {
+        //TODO: Figure out how to handle this.
 
+        // Step 1: Get the current slug and delete it
+        // Step 2: Make a new slug
+        // Step 3: Make a new Revision (like update), but with the new slug
+        // Step 4: Save as normal
     },
 
-    recentActivity: function(slug)
+    recentActivity: function(limit)
     {
-
+        return db.Revision.orderBy('commit.committed').limit(limit).getJoin().run();
     }
 }; // end pages
 
--- server/models.js
+++ server/models.js
@@ -31,20 +31,20 @@ db.User = thinky.createModel('User', {
 
 db.Page = thinky.createModel('Page', {
     created: { _type: Date, default: r.now() }
-}, { pk: 'path' });
+});
 
 //----------------------------------------------------------------------------------------------------------------------
 // Commits
 //----------------------------------------------------------------------------------------------------------------------
 
 db.Commit = thinky.createModel('Commit', {
+    user_id: String,
     message: { _type: String, default: (config.defaultCommit || "minor edit") },
-    committed: { _type: Date, default: r.now() },
-    author_id: String
+    committed: { _type: Date, default: r.now() }
 });
 
 // Relationships
-db.Commit.belongsTo(db.User, "author", "author_id", "email");
+db.Commit.belongsTo(db.User, "user", "user_id", "email");
 
 //----------------------------------------------------------------------------------------------------------------------
 // Revisions
@@ -56,7 +56,8 @@ db.Revision = thinky.createModel('Revision', {
     title: String,
     tags: [String],
     body: String,
-    commit_id: String
+    commit_id: String,
+    deleted: { _type: Boolean, default: false }
 });
 
 // Relationships
@@ -75,7 +76,7 @@ db.Slug = thinky.createModel('Slug', {
 
 // Relationships
 db.Slug.belongsTo(db.Revision, "currentRevision", "currentRevision_id", "id");
-db.Revision.belongsTo(db.Slug, "slug", "slug_id", "id");
+db.Revision.belongsTo(db.Slug, "slug", "slug_id", "url");
 
 //----------------------------------------------------------------------------------------------------------------------
 
